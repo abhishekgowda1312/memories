75658b56b6674c2a1744fb3bd68e08d95111f701
Abhishek Gowda <73712257+abhishekgowda1312@users.noreply.github.com>
2022-01-05 11:22:50 +0530

    Added auth component and refactoring

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Avatar, Button, Paper, Grid, Typography, Container } from '@material-ui/core';
+import useStyles from './styles'
 
 const Auth = () => {
+    const classes = useStyles()
     return (
         <div>
-            AUTH
+
         </div>
     )
 }
